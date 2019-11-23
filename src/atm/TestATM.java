544633776955a11442833bcc4ab936d908198ae1
Emir Mesic <emir.mesic91@yahoo.com>
2019-11-23 12:19:04 +0100

    ArrayList with Created Accounts added in TestATM

--- src/atm/TestATM.java
+++ src/atm/TestATM.java
@@ -1,21 +1,28 @@
 package atm;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestATM {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+
+		ArrayList<Account> createdAccounts = new ArrayList<>();
+
 		Account acc1 = new Account(10, "Emir", 150);
 		Account acc2 = new Account(12, "Mirsad", 270);
 
+		createdAccounts.add(acc1);
+		createdAccounts.add(acc2);
+		
 		System.out.println(acc1.getBalance());
 		acc1.deposit(111);
 		System.out.println(acc1.getBalance());
 		transferMoney(acc1, acc2, 500);
 		System.out.println(acc1.getBalance());
 		System.out.println(acc2.getBalance());
-		
+
 		System.out.println(acc1.toString());
 	}
 
